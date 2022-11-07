7ce89bcc676a50fe2ceddff049c83dfffa1509a7
Mike Kirby <mike.kirby@guildifai.com>
2022-11-06 17:47:03 -0800

    remove reconnect modal popup from inst js files as well.

--- inst/shinyMobile-0.9.1/js/shinyMobile.js
+++ inst/shinyMobile-0.9.1/js/shinyMobile.js
@@ -71,18 +71,6 @@ $((function() {
 }));
 
 $((function() {
-    $(document).on("shiny:disconnected", (function(event) {
-        $("#ss-connect-dialog").hide();
-        $("#ss-overlay").hide();
-        var reconnectToast = app.toast.create({
-            icon: '<i class="icon f7-icons">bolt_fill</i>',
-            position: "center",
-            text: 'Oups... disconnected </br> </br> <div class="row"><button onclick="Shiny.shinyapp.reconnect();" class="toast-button button color-green col">Reconnect</button><button onclick="location.reload();" class="toast-button button color-red col">Reload</button></div>'
-        }).open();
-        $(".toast-button").on("click", (function() {
-            reconnectToast.close();
-        }));
-    }));
     $(document).on("shiny:sessioninitialized", (function(event) {
         Shiny.setInputValue("shinyInfo", Shiny.shinyapp.config);
     }));
